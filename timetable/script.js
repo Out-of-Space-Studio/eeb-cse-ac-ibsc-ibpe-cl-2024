5ba16dd00d6d999c1add3a787f0612f22ee43ee5
JelyFishhhhhh_ _ <tony0940419@gmail.com>
2024-08-30 02:16:30 +0800

    Commit

--- timetable/script.js
+++ timetable/script.js
@@ -3,12 +3,24 @@ window.addEventListener("load", function () {
     var container = document.querySelector(".calendar-container");
 
     function scaleTable() {
-        var scaleX = container.clientWidth / table.scrollWidth;
-        var scaleY = container.clientHeight / table.scrollHeight;
+        // Reset any existing transform
+        table.style.transform = "none";
+
+        var scaleX = container.clientWidth / table.offsetWidth;
+        var scaleY = container.clientHeight / table.offsetHeight;
         var scale = Math.min(scaleX, scaleY);
 
+        // Apply the scale transform
         table.style.transform = `scale(${scale})`;
-        table.style.transformOrigin = "top left";
+
+        // Center the table horizontally and vertically
+        var translateX =
+            (container.clientWidth - table.offsetWidth * scale) / 2;
+        var translateY =
+            (container.clientHeight - table.offsetHeight * scale) / 2;
+        table.style.transform += ` translate(${translateX / scale}px, ${
+            translateY / scale
+        }px)`;
     }
 
     scaleTable();
